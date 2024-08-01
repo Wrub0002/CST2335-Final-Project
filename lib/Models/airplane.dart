30b50bd1002a71b6cb6875d6d5bce827fe938a92
Wrub0002 <Wrub0002@algonquinlive.com>
2024-08-01 13:06:07 -0400

    Created directories and dart files that will be used

---
+++ lib/Models/airplane.dart
@@ -0,0 +1,13 @@
+class Airplane {
+  final String type;
+  final int numberOfPassengers;
+  final int maxSpeed;
+  final int range;
+
+  Airplane({
+    required this.type,
+    required this.numberOfPassengers,
+    required this.maxSpeed,
+    required this.range,
+  });
+}
